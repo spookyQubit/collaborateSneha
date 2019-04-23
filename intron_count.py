9395bac819d67ecf9b34a739041d94997cdc0c71
spookyQubit <iitkgp.shantanu@gmail.com>
2019-04-23 00:43:41 -0400

    split files

--- intron_count.py
+++ intron_count.py
@@ -1,16 +1,21 @@
 import pandas as pd
 
-intron_coordintes_file = "Galaxy_intron_gtf.txt"
+intron_coordintes_file = "data/Galaxy_intron_gtf.txt"
 intron_coordintes_scores_file = "Galaxy_intron_scores_gtf.txt"
-dmso_file = "Galaxy_dmso_bedgraph.txt"
+dmso_file = "data/Galaxy_dmso_bedgraph.txt"
 
 dmso = pd.read_csv(dmso_file, sep='\t')
 
 
-def get_intron_sum(chr_intron, start_intron, end_intron):
-    return dmso[(dmso['start'] >= start_intron)
-                & (dmso['end'] <= end_intron)
-                & (dmso['chr'] == chr_intron)]['dmso_count'].sum()
+#def get_intron_sum(chr_intron, start_intron, end_intron):
+#    return dmso[(dmso['start'] >= start_intron)
+#                & (dmso['end'] <= end_intron)
+#                & (dmso['chr'] == chr_intron)]['dmso_count'].sum()
+
+
+def get_intron_sum(df_chr, start_intron, end_intron):
+    return df_chr[(df_chr['start'] >= start_intron)
+                & (df_chr['end'] <= end_intron)]['dmso_count'].sum()
 
 
 with open(intron_coordintes_file) as f1, \
@@ -22,7 +27,7 @@ with open(intron_coordintes_file) as f1, \
         start_intron, end_intron, zero, sign_, do_, \
         gene_name = line.split('\t')
 
-        intron_sum = get_intron_sum(chr_intron,
+        intron_sum = get_intron_sum(dmso[dmso['chr'] == chr_intron],
                                     int(start_intron),
                                     int(end_intron))
         line_to_write = '\t'.join([chr_intron, bed_, intron_, start_intron, end_intron, zero, sign_, do_, gene_name, str(intron_sum)])
@@ -30,6 +35,8 @@ with open(intron_coordintes_file) as f1, \
 
         f2.write(line_to_write)
         i += 1
+        #if i==5:
+        #    break
         if i%1000 == 0:
             print("Computed {} lines".format(i))
 
---
+++ split_file.py
@@ -0,0 +1,89 @@
+import os
+import pandas as pd
+
+
+g_data_dir = "./data"
+
+g_dmso_parent_file = os.path.join(g_data_dir, "Galaxy_dmso_bedgraph.txt")
+g_dmso_template_file = os.path.join(g_data_dir, "dmso_data/Galaxy_dmso_bedgraph_%s.txt")
+
+g_intron_parent_file = os.path.join(g_data_dir, "Galaxy_intron_gtf.txt")
+g_intron_template_file = os.path.join(g_data_dir, "intron_data/Galaxy_intron_gtf_%s.txt")
+
+g_intron_scores_template_file = os.path.join(g_data_dir, "intron_scores_data/Galaxy_intron_scores_gtf_%s.txt")
+
+
+def split_intron_files_by_chromosome(parent_file):
+    df = pd.read_csv(parent_file, sep='\t')
+    chromosomes = df['chr'].unique()
+    files_per_chromosome = {}
+    for chromosome in chromosomes:
+        file_to_write_to = g_intron_template_file % chromosome
+        if not os.path.isfile(file_to_write_to):
+            df[df['chr']==chromosome].to_csv(file_to_write_to, sep='\t')
+        files_per_chromosome[chromosome] = file_to_write_to
+    return files_per_chromosome
+
+
+def split_dmso_files_by_chromosome(parent_file, chromosomes_in_intron_data):
+    df = pd.read_csv(parent_file, sep='\t')
+    files_per_chromosome = {}
+    for chromosome in chromosomes_in_intron_data:
+        file_to_write_to = g_dmso_template_file % chromosome
+        if not os.path.isfile(file_to_write_to):
+            df[df['chr']==chromosome].to_csv(file_to_write_to, sep='\t')
+        files_per_chromosome[chromosome] = file_to_write_to
+    return files_per_chromosome
+
+
+def get_intron_sum(df_chr, start_intron, end_intron):
+    return df_chr[(df_chr['start'] >= start_intron)
+                & (df_chr['end'] <= end_intron)]['dmso_count'].sum()
+
+
+def write_intron_scores_data(intron_data_file, dmso_data_file, file_to_write):
+
+    print("intron_data_file = {}, dmso_data_file = {}".format(intron_data_file, dmso_data_file))
+
+    df_dmso = pd.read_csv(dmso_data_file, sep='\t')
+    with open(intron_data_file) as f1, \
+            open(file_to_write, 'w') as f2:
+
+        next(f1)  # skip the first line because it is header
+
+        lines = f1.read().splitlines()
+        i = 0
+        for line in lines:
+            idx_, chr_intron, bed_, intron_, \
+            start_intron, end_intron, zero_, sign_, do_, \
+            gene_name = line.split('\t')
+            intron_sum = get_intron_sum(df_dmso,
+                                        int(start_intron),
+                                        int(end_intron))
+
+            line_to_write = '\t'.join(
+                [chr_intron, bed_, intron_, start_intron, end_intron, zero_, sign_, do_, gene_name, str(intron_sum)])
+            line_to_write += '\n'
+
+            f2.write(line_to_write)
+            i += 1
+            if i % 1000 == 0:
+                print("Computed {} lines".format(i))
+
+
+def main():
+    intron_data_files_per_chromosome = split_intron_files_by_chromosome(g_intron_parent_file)
+    chromosomes_in_intron_data = [k for k,v in intron_data_files_per_chromosome.items()]
+    dmso_data_files_per_chromosome = split_dmso_files_by_chromosome(g_dmso_parent_file, chromosomes_in_intron_data)
+
+    files_per_chromosome = {}
+    for chr in intron_data_files_per_chromosome:
+        files_per_chromosome[chr] = (intron_data_files_per_chromosome[chr], dmso_data_files_per_chromosome[chr])
+
+    for chr in files_per_chromosome:
+        intron_data_file, dmso_data_file = files_per_chromosome[chr]
+        write_intron_scores_data(intron_data_file, dmso_data_file, g_intron_scores_template_file%chr)
+
+
+if __name__=="__main__":
+    main()
